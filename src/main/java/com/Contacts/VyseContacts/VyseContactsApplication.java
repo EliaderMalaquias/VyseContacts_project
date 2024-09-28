438197ce1994174a9cc352303374117f61ee53e8
EliaderMalaquias <joseeliader190@gmail.com>
2024-09-28 19:03:39 -0300

    Arquivos Iniciais do Projeto

---
+++ src/main/java/com/Contacts/VyseContacts/VyseContactsApplication.java
@@ -0,0 +1,13 @@
+package com.Contacts.VyseContacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyseContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyseContactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Contacts/VyseContacts/VyseContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Contacts.VyseContacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VyseContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
