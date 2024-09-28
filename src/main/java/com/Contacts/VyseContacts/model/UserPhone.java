e3ee2d288400ae144d97d939a81f36c2942df08f
EliaderMalaquias <joseeliader190@gmail.com>
2024-09-28 19:20:02 -0300

    Criando Entiades e funcionalidades

---
+++ src/main/java/com/Contacts/VyseContacts/model/UserPhone.java
@@ -0,0 +1,32 @@
+package com.Contacts.VyseContacts.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user_phones")
+public class UserPhone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID Id;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String phone;
+
+}
