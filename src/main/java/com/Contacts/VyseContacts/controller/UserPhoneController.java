dacd76335bb85c192313769e4351376d4e317c8d
EliaderMalaquias <joseeliader190@gmail.com>
2024-09-28 22:25:20 -0300

    Implementando funçoes

--- src/main/java/com/Contacts/VyseContacts/controller/UserPhoneController.java
+++ src/main/java/com/Contacts/VyseContacts/controller/UserPhoneController.java
@@ -1,7 +1,22 @@
 package com.Contacts.VyseContacts.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.Contacts.VyseContacts.model.UserPhone;
+import com.Contacts.VyseContacts.service.UserPhoneService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/user")
 public class UserPhoneController {
+
+    @Autowired
+    private UserPhoneService userPhoneService;
+
+    @PostMapping(value = ("/create"))
+    public ResponseEntity createUser(@RequestBody @Valid UserPhone user) {
+       userPhoneService.saveUserPhone(user);
+       return ResponseEntity.ok(user);
+    }
 }
--- src/main/java/com/Contacts/VyseContacts/service/UserPhoneService.java
+++ src/main/java/com/Contacts/VyseContacts/service/UserPhoneService.java
@@ -1,7 +1,19 @@
 package com.Contacts.VyseContacts.service;
 
+import com.Contacts.VyseContacts.model.UserPhone;
+import com.Contacts.VyseContacts.repositories.UserPhoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserPhoneService {
+
+    @Autowired
+    private UserPhoneRepository repository;
+
+    public UserPhone saveUserPhone(UserPhone userPhone) {
+        return repository.save(userPhone);
+    }
+
+
 }
